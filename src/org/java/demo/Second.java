a8b7e61d86c4f7563b05cb0fc8a25b865d183902
chris <1618641@qq.com>
2017-07-05 16:15:54 +0800

    dev-mg添加了文件

--- src/org/java/demo/Second.java
+++ src/org/java/demo/Second.java
@@ -1,11 +1,10 @@
 package org.java.demo;
 
-public class Demo {
+public class Second {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("!111111111111111111111111111111");
-		
+
 	}
 
 }
